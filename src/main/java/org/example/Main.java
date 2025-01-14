e8ec6e84afadc0a49f9205950703373ede71e3ba
Milosz Karpeta <82086059+Milosz-20@users.noreply.github.com>
2025-01-14 18:50:59 +0100

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.example.util.HibernateUtil;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        List<Product> result = session.createQuery("from Product", Product.class).list();
+        result.forEach(product -> {
+            System.out.println(product.getName());
+        });
+
+        transaction.commit();
+        session.close();
+    }
+}
---
+++ src/main/java/org/example/Product.java
@@ -0,0 +1,55 @@
+package org.example;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    Long id;
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "price")
+    Double price;
+
+    @Column(name = "barcode")
+    int barcode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(int barcode) {
+        this.barcode = barcode;
+    }
+}
+
