1ee4fa6f3b881f39c21f024faacdbe9dec030324
KowaP <patrykator.kow@gmail.com>
2025-01-18 21:44:16 +0100

    refactor: Remove unused Hibernate code from Main
    This commit removes unused code from the `Main` class that was previously used for directly interacting with Hibernate and fetching products.
    
    - The `ConsoleApp` class now handles all database interactions.
    - This simplifies the `Main` class and removes unnecessary dependencies.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,6 @@
 package org.example;
 
 import org.example.console.ConsoleApp;
-import org.hibernate.SessionFactory;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
-import org.example.util.HibernateUtil;
-
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,15 +8,3 @@ public class Main {
         consoleApp.run();
     }
 }
-
-//SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-//Session session = sessionFactory.openSession();
-//Transaction transaction = session.beginTransaction();
-//
-//List<Product> result = session.createQuery("from Product", Product.class).list();
-//        result.forEach(product -> {
-//        System.out.println(product.getName());
-//        });
-//
-//        transaction.commit();
-//        session.close();
